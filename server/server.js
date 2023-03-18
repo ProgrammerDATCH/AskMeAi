f5f0d05076e31df6a800d51911ed8b2e4985ec63
Programmer DATCH <programmerdatch@gmail.com>
2023-03-18 19:17:01 +0200

    test2

--- server/server.js
+++ server/server.js
@@ -23,29 +23,31 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
     try {
-        const prompt = req.body.prompt;
-        const history = req.body.history || [];
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${history.join('\n')}\n${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-        });
-
-        const botResponse = response.data.choices[0].text;
-        const newHistory = [...history, `${prompt}\n${botResponse}`];
-
-        res.status(200).send({
-            bot: botResponse,
-            history: newHistory,
-        });
+      const prompt = req.body.prompt;
+      const conversationHistory = req.body.conversationHistory || [];
+  
+      const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${conversationHistory.join('\n')}\nUser: ${prompt}\nAI:`,
+        temperature: 0,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+      });
+  
+      const botResponse = response.data.choices[0].text.trim();
+      conversationHistory.push(`User: ${prompt}\nAI: ${botResponse}`);
+  
+      res.status(200).send({
+        bot: botResponse,
+        conversationHistory,
+      });
     } catch (error) {
-        console.log(error);
-        res.status(500).send({ error });
+      console.log(error);
+      res.status(500).send({ error });
     }
-});
+  });
+  
 
 app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
