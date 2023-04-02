f23078cebc1c5d9e16fea3ad37d3ce03a4576633
Programmer DATCH <programmerdatch@gmail.com>
2023-04-02 09:43:32 +0200

    change3

--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
+if (!process.env.OPENAI_API_KEY) {
+    console.error('OPENAI_API_KEY environment variable is missing');
+    process.exit(1);
+}
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -15,36 +20,65 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const sessions = {};
+
 app.get('/', async (req, res) => {
     res.status(200).send({
         message: 'DATCH Ai Server is well.',
-    })
+    });
 });
 
 app.post('/', async (req, res) => {
     try {
-      const prompt = req.body.prompt;
-  
-      const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${prompt}`,
-        temperature: 0,
-        max_tokens: 3000,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-      });
-  
-      const botResponse = response.data.choices[0].text.trim();
+        const userId = req.body.userId;
+        const prompt = req.body.prompt;
 
-      res.status(200).send({
-        bot: botResponse,
-      });
+        if (!userId || typeof userId !== 'string') {
+            return res.status(400).send({ error: 'userId is missing or invalid' });
+        }
+
+        if (!prompt || typeof prompt !== 'string') {
+            return res.status(400).send({ error: 'prompt is missing or invalid' });
+        }
+
+        if (!sessions[userId]) {
+            sessions[userId] = {
+                history: '',
+            };
+        }
+
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `${sessions[userId].history} ${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+
+        const botResponse = response.data.choices[0].text.trim();
+
+        sessions[userId].history += ` ${prompt} ${botResponse}`;
+
+        res.status(200).send({
+            bot: botResponse,
+        });
     } catch (error) {
-      console.log(error);
-      res.status(500).send({ error });
+        console.log(error);
+        res.status(500).send({ error });
     }
-  });
-  
+});
 
-app.listen(5000, () => console.log('DATCH Ai Server is running well... on http://localhost:5000'));
+const port = process.env.PORT || 5000;
+const maxRequestsPerMinute = process.env.MAX_REQUESTS_PER_MINUTE || 60;
+
+app.listen(port, () => console.log(`DATCH Ai Server is running on http://localhost:${port}`));
+
+// Add rate limiting middleware to limit the number of requests per minute
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: maxRequestsPerMinute, // limit each IP to maxRequestsPerMinute requests per windowMs
+});
+app.use(limiter);
